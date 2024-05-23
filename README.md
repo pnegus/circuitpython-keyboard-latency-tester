ef8655c7bcbaf32d4d48a7364d88a54b18cc4d5f
pnegus <negus.patrickyi@gmail.com>
2024-05-23 00:10:05 -0400

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # circuitpython-keyboard-latency-tester
 code for using circuitpython esp32 to test mech keyboard latency
+
+## Overview
+
+`code.py` programs your esp32 to-- on the press of the debug button-- send an 'f' keypress followed by setting the 'g' switch contact high.
+
+`program.py` intercepts these inputs and simply prints the time differential between the two. This in theory should allow for comparing PCB latency between various keyboards.
+
+## Usage
+
+- Load `code.py` to your circuitpython board
+
+- Run `program.py`
---
+++ code.py
@@ -0,0 +1,60 @@
+import board
+import digitalio
+from digitalio import DigitalInOut, Direction, Pull
+from analogio import AnalogIn
+import time
+from adafruit_hid.mouse import Mouse
+import usb_hid
+
+light_sensor = AnalogIn(board.D11)
+led = digitalio.DigitalInOut(board.LED)
+led.direction = digitalio.Direction.OUTPUT
+
+btn = DigitalInOut(board.BUTTON)
+btn.direction = Direction.INPUT
+btn.pull = Pull.UP
+
+mouse = Mouse(usb_hid.devices)
+
+while True:
+    if not btn.value:
+        led.value = False
+        #give 3 seconds to let sensor sit on display
+        time.sleep(3)
+        
+        #evaluate average luminance, ideally measures black level
+        #TODO: need to determine effective sampling rate. ALS-PT19 rise/fall time is .11 and .22 ms respectively (idk what that means)
+        start_calibration_timestamp = time.monotonic()
+        initial_sensorvalue = 0.0
+        n = 0
+        while (time.monotonic() - start_calibration_timestamp <= 3.0):
+            initial_sensorvalue += light_sensor.value
+            n += 1
+        initial_sensorvalue = initial_sensorvalue / n
+
+        average_latency = 0.0
+        num_runs = 50
+        num_miss = 0
+        
+        for i in range(num_runs):
+            mouse.press(Mouse.LEFT_BUTTON)
+            time.sleep(0.01)
+            mouse.release(Mouse.LEFT_BUTTON)
+            start_test_timestamp = time.monotonic()
+            #gtg2% question mark?
+            #might need to clamp sampling frequency
+            while (time.monotonic() - start_test_timestamp <= 0.1 and light_sensor.value <= initial_sensorvalue + 1.6 * initial_sensorvalue):
+                continue
+            latency = (time.monotonic() - start_test_timestamp) * 1000
+            if latency >= 0.1:
+                #ignore
+                num_miss += 1
+            else:
+                average_latency += latency
+            time.sleep(0.25)
+        
+        print("Average latency over all runs:")
+        print(average_latency / (num_runs - num_miss))
+    else:
+        led.value = True
+    
