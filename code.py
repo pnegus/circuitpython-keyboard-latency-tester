d6ebb1379b9f265e65c93bba6ffa44b61ac9a10b
pnegus <51723374+pnegus@users.noreply.github.com>
2024-05-29 21:41:54 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,60 +1,35 @@
+# Write your code here :-)
 import board
 import digitalio
-from digitalio import DigitalInOut, Direction, Pull
-from analogio import AnalogIn
+from digitalio import DigitalInOut, Direction, Pull, DriveMode
 import time
-from adafruit_hid.mouse import Mouse
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
 import usb_hid
 
-light_sensor = AnalogIn(board.D11)
-led = digitalio.DigitalInOut(board.LED)
-led.direction = digitalio.Direction.OUTPUT
+gate = DigitalInOut(board.D11)
+gate.direction = Direction.OUTPUT
+gate.value = False
+
+led = DigitalInOut(board.LED)
+led.direction = Direction.OUTPUT
 
 btn = DigitalInOut(board.BUTTON)
 btn.direction = Direction.INPUT
 btn.pull = Pull.UP
 
-mouse = Mouse(usb_hid.devices)
+keyboard = Keyboard(usb_hid.devices)
 
 while True:
     if not btn.value:
         led.value = False
-        #give 3 seconds to let sensor sit on display
-        time.sleep(3)
-        
-        #evaluate average luminance, ideally measures black level
-        #TODO: need to determine effective sampling rate. ALS-PT19 rise/fall time is .11 and .22 ms respectively (idk what that means)
-        start_calibration_timestamp = time.monotonic()
-        initial_sensorvalue = 0.0
-        n = 0
-        while (time.monotonic() - start_calibration_timestamp <= 3.0):
-            initial_sensorvalue += light_sensor.value
-            n += 1
-        initial_sensorvalue = initial_sensorvalue / n
-
-        average_latency = 0.0
-        num_runs = 50
-        num_miss = 0
-        
-        for i in range(num_runs):
-            mouse.press(Mouse.LEFT_BUTTON)
-            time.sleep(0.01)
-            mouse.release(Mouse.LEFT_BUTTON)
-            start_test_timestamp = time.monotonic()
-            #gtg2% question mark?
-            #might need to clamp sampling frequency
-            while (time.monotonic() - start_test_timestamp <= 0.1 and light_sensor.value <= initial_sensorvalue + 1.6 * initial_sensorvalue):
-                continue
-            latency = (time.monotonic() - start_test_timestamp) * 1000
-            if latency >= 0.1:
-                #ignore
-                num_miss += 1
-            else:
-                average_latency += latency
-            time.sleep(0.25)
-        
-        print("Average latency over all runs:")
-        print(average_latency / (num_runs - num_miss))
+        for i in range(20):
+            time.sleep(1)
+            keyboard.press(Keycode.F)
+            keyboard.release(Keycode.F)
+            gate.value = True
+            time.sleep(0.001)
+            gate.value = False
     else:
         led.value = True
-    
+
